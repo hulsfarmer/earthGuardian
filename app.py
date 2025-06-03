ccf222063e3b65499b41ea65ecefde12e5bf984a
hulsfarmer <huls_family@naver.com>
2025-06-03 10:32:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory, abort, redirect, url_for
 import json
 from datetime import datetime, timedelta, timezone
 import logging
